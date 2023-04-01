62303fc91be556f8f48a19216fa4d2057e6399f6
Brennan Walsh <mail@brennanwal.sh>
2023-04-01 14:28:27 +0000

    configuring rules.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true
-	},
-	extends: ['eslint:recommended', 'plugin:import/recommended'],
-	parser: '@babel/eslint-parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module'
-	},
-	rules: {
-		'import/no-duplicates': 'off'
-	}
-}
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "array-callback-return": "warn",
+    "no-await-in-loop": "warn",
+    "no-constant-binary-expression": "error",
+    "no-constructor-return": "error",
+    "no-duplicate-imports": "warn",
+    "no-new-native-nonconstructor": "error",
+    "no-promise-executor-return": "error",
+    "no-self-compare": "warn",
+    "no-template-curly-in-string": "warn",
+  },
+};
